4368f0b37420578c3f8619cd360346264b0be929
Peter2220 <56364669+Peter2220@users.noreply.github.com>
2020-01-07 15:26:00 +0200

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,33 @@
+## The second line is used in order for the   floor   function to work well.
+from math import *
+
+print (10 % 3)  ## It will print the remainder which is: 1
+
+
+num = 1
+print(str(num)) ## Integer will be a String
+
+
+## print(num + " Is my favorite number") ## ERROR
+
+print((str(num) + " Is my favorite number")) ## 1 Is my favorite number
+
+
+My_fav_num = -1
+print(abs(My_fav_num))  ## 1
+
+print(pow(4,6))    ## (4)^6 -> 4096.0       Float not Integer
+
+print(max(4,6))    ## 6
+      
+print(min(2,50))   ## 2
+
+print(round(3.2))  ## 3
+
+print(round(3.7))  ## 4
+
+print(floor(3.7))  ## 3
+
+print(ceil(3.7))   ## 4
+
+print(sqrt(25))    ## 5.0                    Float not Integer
